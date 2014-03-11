8114957990628514b80e0305ba8ac5bb94a409b8
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2014-03-11 11:40:16 +0100

    Added some more files to the hierarchy.

---
+++ src/Engine/Board.hpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+using std::vector;
+
+/**
+ * @brief Represents a board in the game.
+ * Has to be able to perform moves and return info about the state of the game.
+ * It needs to remember the last player.
+ * 
+ */
+class Board {
+public:
+	
+private:
+	//TODO neighbours
+};
---
+++ src/Engine/constants.hpp
@@ -0,0 +1,5 @@
+#include <utility>
+
+using std::pair;
+
+const pair<int, int> directions = {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}};
