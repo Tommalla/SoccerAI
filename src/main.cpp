a35d5879b0b8b5e57bf52d86957777d898ac46f1
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2014-04-02 19:08:00 +0200

    Further GTP implementation. Refs #1.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <sstream>
 #include <cassert>
+#include <memory>
+#include <bits/algorithmfwd.h>
 
 #include "AI/AI.hpp"
 
@@ -15,7 +17,14 @@ inline void printNotSupported() {
 	cout << "?\n\n";
 }
 
+DirId dirMap[] = {Board::RIGHT, Board::RIGHT_UP, Board::UP, Board::LEFT_UP, Board::LEFT,
+		  Board::LEFT_DOWN, Board::DOWN, Board::RIGHT_DOWN};
+DirId reverseDirMap[8];
+
 int main() {
+	for (int i = 0; i < 8; ++i)
+		reverseDirMap[dirMap[i]] = i;
+
 	string line;
 	string command;
 
@@ -25,14 +34,19 @@ int main() {
 	cin >> w >> h;
 	printOK();
 
+	unique_ptr<AI> ai; //= unique_ptr<AI>(new SomeAI(w, h, params));
+
 	while (getline(cin, line)) {
 		stringstream ss(line);
 		ss >> command;
 
 		if (command == "play") {
+			DirId d;
+			ss >> d;
+			ai->play(dirMap[d]);
 			printNotSupported();
 		} else if (command == "gen_move") {
-			printNotSupported();
+			cout << reverseDirMap[ai->genMove()] << "\n\n";
 		} else if (command == "time_left") {
 			printNotSupported();
 		} else if (command == "undo") {
