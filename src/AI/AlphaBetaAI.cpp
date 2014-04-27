Tommalla/SoccerAI

commit d60403bf1085487a6e3936377d00c5774138979a

Added AI::generateMove(), removing the compilation warnings. Closes #20.

src/AI/AlphaBetaAI.cpp
@@ -15,7 +15,7 @@ AlphaBetaAI::AlphaBetaAI(const engine::Coord width, const engine::Coord height, 
 , fieldsUsed{0}
 , fields{(width + 1) * (height + 1)} {}
 
-DirId AlphaBetaAI::genMove() {
+DirId AlphaBetaAI::generateMove() {
 	AI::genMove();
 	Board copy{board};
 	auto moves = board.getMoves();

src/AI/AI.cpp
@@ -20,6 +20,7 @@ void AI::play(const DirId& move) {
 
 DirId AI::genMove() {
 	alreadyMoved = true;
+	return generateMove();
 }
 
 void AI::undo() {

src/AI/AI.hpp
@@ -18,7 +18,7 @@ public:
 	 * @brief Generates the next move.
 	 * This method **has** to be implemented.
 	 */
-	virtual DirId genMove();
+	DirId genMove();
 	/**
 	 * @brief Undoes the last move.
 	 */
@@ -29,6 +29,8 @@ public:
 	virtual void setTimeLeft(const int time);
 
 protected:
+	virtual DirId generateMove() = 0;
+
 	std::stack<std::pair<DirId, bool>> history;
 	Board board;
 	int timeLeft, lastTimeLeft, lastMoveTime;

src/AI/AlphaBetaAI.hpp
@@ -7,9 +7,9 @@
 class AlphaBetaAI : public AI {
 public:
 	AlphaBetaAI(const engine::Coord width, const engine::Coord height, const std::function<int(const Board&)>& value);
-	virtual DirId genMove();
 	virtual void play(const DirId& move);
 private:
+	virtual DirId generateMove();
 	int gen(Board& s, int alpha, int beta, const unsigned int depth);
 
 	std::function<int(const Board&)> value;

src/AI/RandomAI.cpp
@@ -6,7 +6,7 @@ RandomAI::RandomAI(const engine::Coord& width, const engine::Coord& height)
 : AI(width, height) {}
 
 
-DirId RandomAI::genMove() {
+DirId RandomAI::generateMove() {
 	static std::mt19937 mt{std::random_device{}()};
 	AI::genMove();
 	auto possible = board.getMoves();

src/AI/RandomAI.hpp
@@ -6,7 +6,9 @@
 class RandomAI: public AI {
 public:
 	RandomAI(const engine::Coord& width, const engine::Coord& height);
-	virtual DirId genMove();
+
+protected:
+	virtual DirId generateMove();
 };
 
 #endif // RANDOM_AI_HPP

src/main.cpp
@@ -10,6 +10,7 @@
 #include "AI/AlphaBetaAI.hpp"
 #include "AI/objectiveFunctions.hpp"
 
+// #define RANDOM_AI
 #define ALPHA_BETA_AI
 
 using namespace std;

commit c3cf5464f993d59a4ba5f6c0fa951b8018b4e135

Fixed a bug introduced in d60403bf108 . Refs #20.

src/AI/AlphaBetaAI.cpp
@@ -16,7 +16,6 @@ AlphaBetaAI::AlphaBetaAI(const engine::Coord width, const engine::Coord height, 
 , fields{(width + 1) * (height + 1)} {}
 
 DirId AlphaBetaAI::generateMove() {
-	AI::genMove();
 	Board copy{board};
 	auto moves = board.getMoves();
 	int bestVal = -1;