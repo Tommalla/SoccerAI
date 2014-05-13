8eb608d9a1cab62d7ff4298a7309aff8a2140117
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2014-05-13 18:57:26 +0200

    Added a test for hashing.

---
+++ src/Engine/tests/HashTest.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <set>
+#include <ctime>
+#include <cstdlib>
+#include <cassert>
+
+#include "../Board.hpp"
+
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+	for (int i = 0; i < 100000; ++i) {
+		Board b{8, 10};
+		set<engine::Hash> s;
+		s.insert(b.getHash());
+		while (!b.isGameFinished()) {
+			auto moves = b.getMoves();
+			b.play(moves[rand() % moves.size()]);
+			assert(s.find(b.getHash()) == s.end());
+			s.insert(b.getHash());
+		}
+	}
+	return 0;
+}
